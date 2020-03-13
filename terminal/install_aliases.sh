0e5a8cb4cfbac4377de7fa97839b69a52403ce34
Sam <sam@bigeyedeers.co.uk>
2020-03-13 09:25:20 +0000

    Fixed

--- terminal/install_aliases.sh
+++ terminal/install_aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIVIDER="-------------------------"
-INJECTION="\n# SAM SCRIPTS\nfor f in /opt/sam/terminal/enabled/*.conf; do source $f; done\n# END SAM SCRIPTS"
+INJECTION="\n# SAM SCRIPTS\nfor f in /opt/sam/terminal/enabled/*.conf; do source \$f; done\n# END SAM SCRIPTS"
 echo $INJECTION >> ~/.zprofile
 echo $INJECTION >> ~/.bash_profile
 
