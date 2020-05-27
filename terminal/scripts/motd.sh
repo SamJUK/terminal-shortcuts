d1bb1388a2dfd47d651a4049a1c7f9ed3f1fddca
Sam James <sam@samdjames.uk>
2020-05-27 09:55:58 +0100

    added neofetch replacement

--- terminal/scripts/motd.sh
+++ terminal/scripts/motd.sh
@@ -1,4 +1,10 @@
 cat $(ls -d /opt/bed/terminal/scripts/motd/*.txt | sort -R | tail -n1)
+if ! [ -x "$(command -v neofetch)" ]; then
+    cat $(ls -d /opt/sam/terminal/motd/*.txt | sort -R | tail -n1)
+else
+    neofetch --color_blocks off
+fi
+
 echo "==================="
 date
 sh /opt/bed/terminal/scripts/ip.sh
